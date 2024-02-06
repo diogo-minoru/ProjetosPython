6fb835d39ac2c3a4730f7bcf8cc000e2377adfb4
diogo-minoru <132161289+diogo-minoru@users.noreply.github.com>
2024-02-06 20:01:46 -0300

    removendo linha desnecessárias

--- web_scraping/empregos.py
+++ web_scraping/empregos.py
@@ -1,24 +1,31 @@
+"""
+    Programa para acessar o site de empregos "maringá.com" e listar todos os empregos anunciados pelas empresas
+    Os anúncios são armazenados em um dataframe e é salvo um arquivo em .xlsx com todos os resultados obtidos
+"""
+
 from requests_html import HTMLSession
-from datetime import date
 import pandas as pd
 import time
 
 session = HTMLSession()
-data_frame = list()
+jobs_list = list()
 
+# Definindo até qual página será percorrido
+LAST_PAGE = 10
+
+def main():
+    return parse_jobs(parse_page_links())
+
+# Criando uma lista com todos os links que deverá ser percorrido
 def parse_page_links():
-    base_url = "https://empregos.maringa.com/?area=&bairro=&cidade=&estado=&experiencia=&faixa_salarial=&text=&vagas-de-emprego={1}"
-    response = session.get(base_url)
-    last_element = len(response.html.find("a.page-link.rounded[href]"))
-    last_page = 10#int(response.html.find("a.page-link.rounded[href]")[last_element - 1].attrs["href"][-3:])
     all_links = list()
-    for page in range(1, last_page + 1):
+    for page in range(1, LAST_PAGE + 1):
         url = f"https://empregos.maringa.com/?area=&bairro=&cidade=&estado=&experiencia=&faixa_salarial=&text=&vagas-de-emprego={page}"
         all_links.append(url)
     return all_links
 
 def parse_jobs(urls):
-    for url in urls[0:15]:
+    for url in urls:
         response = session.get(url)
         size = len(response.html.find("div.card-anuncio.mb-3"))
         for i in range(0, size):
@@ -29,14 +36,16 @@ def parse_jobs(urls):
                 "publication_date": response.html.find("small.text-nowrap.ml-4")[i].text,
                 "job_link": response.html.find("a.flex-wrap[href]")[i].attrs["href"]
             }
-            data_frame.append(job)
+            jobs_list.append(job)
         time.sleep(1)
 
-parse_jobs(parse_page_links())
-df = pd.DataFrame(data_frame).query("publication_date.str.len() > 0").reset_index(drop = True)
-df["publication_date"] = pd.to_datetime(df["publication_date"]).dt.strftime("%d/%m/%Y %H:%M")
+if __name__ == "__main__":
+    main()
+
+df = pd.DataFrame(jobs_list)
+df["publication_date"] = pd.to_datetime(df["publication_date"], format = "%d/%m/%Y %H:%M").dt.strftime("%d/%m/%Y %H:%M")
 df["job_title"] = df["job_title"].str.lower()
 df = df[df["job_title"].str.contains("analista|dados|dado|b.i.|b.i|power|inteligência|inteligencia|business|intelligence")]
-#df.info()
+
 df.to_excel("C:\\Users\\diogo\\Downloads\\empregos.xlsx", sheet_name = "empregos")
 print(df)
